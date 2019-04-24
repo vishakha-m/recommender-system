c0f2ee544ddddeb14f41ef8250f43c52e297464e
Vishakha M <vishakha6321@gmail.com>
2019-04-24 12:55:46 +0530

    Add scripts for cleaning dataset

---
+++ scripts/dataset_to_csv.py
@@ -0,0 +1,21 @@
+# Dataset should be in current directory with the name "dataset.json"
+import csv,json
+import  re
+file = open('dataset.json','r')
+sample = json.loads(file.readline().rstrip())
+del sample['reviewTime']
+fieldnames=list(json.loads(file.readline().rstrip()).keys())  
+out = csv.DictWriter(open('reviews.csv','w'),fieldnames)
+file = open('dataele.json','r')
+out.writeheader()
+pat = re.compile('[^A-Za-z0-9\s]+')
+for line in file: 
+	obj = json.loads(line.rstrip()) 
+	try:
+		del obj['reviewTime']
+		obj['summary'] = re.sub(pat, '', obj['summary'])
+		obj['reviewText'] = re.sub(pat, '', obj['reviewText'])
+		obj['helpful'] = obj['helpful'][0]/(obj['helpful'][0]+1)
+		out.writerow(obj) 
+	except KeyError:
+		print(obj,line)
